8027f087f163d0c6acf4c4d962d8d3e1034546be
Lakshminarayana Nekkanti <narayana.nekkanti@gmail.com>
2024-05-26 10:48:35 +0530

    changes 6

--- jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/internal/jpa3/resource/java/AccessAnnotationDefinition3_0.java
+++ jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/internal/jpa3/resource/java/AccessAnnotationDefinition3_0.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 Oracle. All rights reserved.
+ * Copyright (c) 2009, 2013 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0, which accompanies this distribution
  * and is available at https://www.eclipse.org/legal/epl-2.0/.
@@ -7,25 +7,24 @@
  * Contributors:
  *     Oracle - initial API and implementation
  ******************************************************************************/
-package org.eclipse.jpt.jpa.core.internal.jpa2_1.resource.java;
+package org.eclipse.jpt.jpa.core.internal.jpa3.resource.java;
 
 import org.eclipse.jdt.core.IAnnotation;
-import org.eclipse.jpt.common.core.internal.resource.java.binary.BinaryNamedAnnotation;
-import org.eclipse.jpt.common.core.internal.resource.java.source.SourceNamedAnnotation;
 import org.eclipse.jpt.common.core.resource.java.Annotation;
 import org.eclipse.jpt.common.core.resource.java.AnnotationDefinition;
 import org.eclipse.jpt.common.core.resource.java.JavaResourceAnnotatedElement;
 import org.eclipse.jpt.common.core.utility.jdt.AnnotatedElement;
-import org.eclipse.jpt.jpa.core.jpa2_1.resource.java.JPA2_1;
+import org.eclipse.jpt.jpa.core.internal.jpa2.resource.java.binary.BinaryAccessAnnotation2_0;
+import org.eclipse.jpt.jpa.core.internal.jpa2.resource.java.source.SourceAccessAnnotation2_0;
+import org.eclipse.jpt.jpa.core.jpa2.resource.java.AccessAnnotation2_0;
+import org.eclipse.jpt.jpa.core.jpa3.resource.java.AccessAnnotation3_0;
 
 /**
- * <code>javax.persistence.NamedStoredProcedureQueries</code>
+ * <code>javax.persistence.Access</code>
  */
-public final class NamedStoredProcedureQueriesAnnotationDefinition2_1
-	implements AnnotationDefinition
-{
+public final class AccessAnnotationDefinition3_0 implements AnnotationDefinition {
 	// singleton
-	private static final AnnotationDefinition INSTANCE = new NamedStoredProcedureQueriesAnnotationDefinition2_1();
+	private static final AnnotationDefinition INSTANCE = new AccessAnnotationDefinition3_0();
 
 	/**
 	 * Return the singleton.
@@ -37,23 +36,23 @@ public final class NamedStoredProcedureQueriesAnnotationDefinition2_1
 	/**
 	 * Ensure single instance.
 	 */
-	private NamedStoredProcedureQueriesAnnotationDefinition2_1() {
+	private AccessAnnotationDefinition3_0() {
 		super();
 	}
 
 	public Annotation buildAnnotation(JavaResourceAnnotatedElement parent, AnnotatedElement annotatedElement) {
-		return new SourceNamedAnnotation(parent, annotatedElement, getAnnotationName());
+		return new SourceAccessAnnotation2_0(parent, annotatedElement);
 	}
 
 	public Annotation buildNullAnnotation(JavaResourceAnnotatedElement parent) {
-		throw new UnsupportedOperationException();
+		return new NullAccessAnnotation2_0(parent);
 	}
 
 	public Annotation buildAnnotation(JavaResourceAnnotatedElement parent, IAnnotation jdtAnnotation) {
-		return new BinaryNamedAnnotation(parent, jdtAnnotation, getAnnotationName());
+		return new BinaryAccessAnnotation2_0(parent, jdtAnnotation);
 	}
 
 	public String getAnnotationName() {
-		return JPA2_1.NAMED_STORED_PROCEDURE_QUERIES;
+		return AccessAnnotation3_0.ANNOTATION_NAME;
 	}
 }
--- jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/internal/jpa3/resource/java/AttributeOverridesAnnotationDefinition3_0.java
+++ jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/internal/jpa3/resource/java/AttributeOverridesAnnotationDefinition3_0.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 Oracle. All rights reserved.
+ * Copyright (c) 2009, 2011 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0, which accompanies this distribution
  * and is available at https://www.eclipse.org/legal/epl-2.0/.
@@ -7,7 +7,7 @@
  * Contributors:
  *     Oracle - initial API and implementation
  ******************************************************************************/
-package org.eclipse.jpt.jpa.core.internal.jpa2_1.resource.java;
+package org.eclipse.jpt.jpa.core.internal.jpa3.resource.java;
 
 import org.eclipse.jdt.core.IAnnotation;
 import org.eclipse.jpt.common.core.internal.resource.java.binary.BinaryNamedAnnotation;
@@ -16,16 +16,15 @@ import org.eclipse.jpt.common.core.resource.java.Annotation;
 import org.eclipse.jpt.common.core.resource.java.AnnotationDefinition;
 import org.eclipse.jpt.common.core.resource.java.JavaResourceAnnotatedElement;
 import org.eclipse.jpt.common.core.utility.jdt.AnnotatedElement;
-import org.eclipse.jpt.jpa.core.jpa2_1.resource.java.JPA2_1;
+import org.eclipse.jpt.jpa.core.resource.java.JPA;
+import org.eclipse.jpt.jpa.core.resource.persistence.v3_0.JPA3_0;
 
 /**
- * <code>javax.persistence.NamedStoredProcedureQueries</code>
+ * javax.persistence.AttributeOverrides
  */
-public final class NamedStoredProcedureQueriesAnnotationDefinition2_1
-	implements AnnotationDefinition
-{
+public final class AttributeOverridesAnnotationDefinition3_0 implements AnnotationDefinition {
 	// singleton
-	private static final AnnotationDefinition INSTANCE = new NamedStoredProcedureQueriesAnnotationDefinition2_1();
+	private static final AnnotationDefinition INSTANCE = new AttributeOverridesAnnotationDefinition3_0();
 
 	/**
 	 * Return the singleton.
@@ -37,7 +36,7 @@ public final class NamedStoredProcedureQueriesAnnotationDefinition2_1
 	/**
 	 * Ensure single instance.
 	 */
-	private NamedStoredProcedureQueriesAnnotationDefinition2_1() {
+	private AttributeOverridesAnnotationDefinition3_0() {
 		super();
 	}
 
@@ -54,6 +53,6 @@ public final class NamedStoredProcedureQueriesAnnotationDefinition2_1
 	}
 
 	public String getAnnotationName() {
-		return JPA2_1.NAMED_STORED_PROCEDURE_QUERIES;
+		return JPA3_0.ATTRIBUTE_OVERRIDES;
 	}
 }
